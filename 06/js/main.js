37f4ff5752b50c9bd5c872eb7ba36f330b1b5efd
A.Shevchenko <5b4e5f58555f4e5b@gmail.com>
2021-02-16 19:58:39 +0200

    1-4 tasks is complete

---
+++ 06/js/main.js
@@ -0,0 +1,17 @@
+// First task
+var array0 = ['a', 'b', 'c'];
+var array1 = [1, 2, 3];
+console.log(array0.concat(array1));
+
+// Second task
+console.log(array1.reverse());
+
+//Third task
+console.log([3, 4, 1, 2, 7].sort());
+
+//Fourth task
+var array2 = [3, 4, 1, 2, 7, 5, 8];
+
+var evenArray = array2.filter(function (value) { return value % 2 === 0; });
+
+console.log(evenArray);
