289b9e9169446f52c919e595fb58222e658f06c7
A.Shevchenko <5b4e5f58555f4e5b@gmail.com>
2021-02-20 18:53:06 +0200

    Second task is done

--- 07/js/main.js
+++ 07/js/main.js
@@ -1,5 +1,15 @@
 // First Task
-var numbers = [-5, 4, 2, 10, 15, 22, -5, -11, -1, 0, -9];
+var numbers = [4, 2, 10, 15, 22, -5, -11, -1, 0, -9];
 
 var result = numbers.filter(value => value > 0).map(value => Math.sqrt(value));
 console.log(result);
+
+//Second Task
+var count = 0;
+
+numbers.reduce(function (acc, cur, index) {
+    if(acc > 10 && count === 0) count = index;
+    return acc + cur;
+});
+
+console.log(count);
