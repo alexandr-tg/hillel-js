c6df12188528cf3ae82795f297865f16459c3e2e
A.Shevchenko <5b4e5f58555f4e5b@gmail.com>
2021-02-20 22:04:07 +0200

    First task is done

---
+++ 08/js/main.js
@@ -0,0 +1,10 @@
+// First task
+var abc = 'aaa bbb ccc';
+
+var subStr = abc.substr(4, 3);
+var subString = abc.substring(4,7);
+var slice = abc.slice(4,7);
+
+console.log(subStr);
+console.log(subString);
+console.log(slice);
