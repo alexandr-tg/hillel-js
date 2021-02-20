489406a86eff9abcc2ad9e4e3c1a36812922dfcb
A.Shevchenko <5b4e5f58555f4e5b@gmail.com>
2021-02-20 22:18:08 +0200

    Second Task is done

--- 08/js/main.js
+++ 08/js/main.js
@@ -8,3 +8,8 @@ var slice = abc.slice(4,7);
 console.log(subStr);
 console.log(subString);
 console.log(slice);
+
+// Second task
+var iLearnJS = 'я учу javascript!';
+var capCharStr = iLearnJS.charAt(0).toUpperCase() + iLearnJS.slice(1);
+console.log(capCharStr);
