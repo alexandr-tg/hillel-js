49bd8c8c3421568ce9074c4b4370669d7925288b
A.Shevchenko <5b4e5f58555f4e5b@gmail.com>
2021-02-27 20:38:41 +0200

    Second task is done

--- 09/js/main.js
+++ 09/js/main.js
@@ -1,4 +1,12 @@
 // First task
 var obj = {'Коля':'1000', 'Вася':'500', 'Петя':'200'};
 
-document.write(`${obj['Петя']} ${obj['Коля']}`);
+document.write(`${obj['Петя']} ${obj['Коля']} <br>`);
+
+//Second task
+var arr = {
+    'ru':['голубой', 'красный', 'зеленый'],
+    'en':['blue', 'red', 'green'],
+};
+
+document.write(arr['ru'][0]);
