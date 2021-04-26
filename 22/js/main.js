61cf5797c8a7841bc0ae5d4fe480f2160a9cf4af
A.Shevchenko <5b4e5f58555f4e5b@gmail.com>
2021-04-26 18:39:04 +0300

    task is done

---
+++ 22/js/main.js
@@ -0,0 +1,9 @@
+fetch('https://api.github.com/users')
+    .then((response) => response.json())
+    .then(data => {
+        for (var i = 0; i < 10; i++) {
+            console.log(data[i].login);
+            console.log(data[i].avatar_url);
+            console.log(data[i].id);
+        }
+    });
