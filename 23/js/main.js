6d97a80ac61dfc6d7f4c37d838e6f0e16111a442
A.Shevchenko <5b4e5f58555f4e5b@gmail.com>
2021-04-29 17:04:33 +0300

    Working prototype is done

--- 23/js/main.js
+++ 23/js/main.js
@@ -1,16 +1,27 @@
+var userData = {};
+const activityTable = {
+    1: 1,
+    2: 1.3,
+    3: 1.5
+};
+
 var buttons = document.getElementsByTagName('button');
 var gender = document.querySelectorAll('input[name=gender]');
 var currentHeight = document.querySelector('input[name=height]');
 var currentWeight = document.querySelector('input[name=weight]');
 var wishfulWeight = document.querySelector('input[name=wishful-weight]');
 var userActivity = document.querySelector('input[name=activity]');
+var wishfulResult = document.querySelector('input[name=wish-result]');
 
-currentHeight.addEventListener('input', () => showRangeValue('span[name=height]'));
-showRangeValue('span[name=height]');
+currentHeight.addEventListener('input', () => showHeightValue('span[name=height]'));
+showHeightValue('span[name=height]');
 
-currentWeight.addEventListener('input', getWeight);
-wishfulWeight.addEventListener('input', getWeight);
+currentWeight.addEventListener('input', setWeight);
+wishfulWeight.addEventListener('input', setWishfulWeight);
+currentWeight.addEventListener('keyup', numCheck);
+wishfulWeight.addEventListener('keyup', numCheck);
 userActivity.addEventListener('input', showActivity);
+wishfulResult.addEventListener('input', showWishfulResult);
 
 
 for (var i = 0; i < buttons.length; i++){
@@ -18,7 +29,11 @@ for (var i = 0; i < buttons.length; i++){
 }
 
 for (var i = 0; i < gender.length; i++){
-    gender[i].addEventListener('click', getGenderValue);
+    gender[i].addEventListener('click', setGenderValue);
+}
+
+function numCheck(event){
+   event.target.value = event.target.value.replace(/[^\d]/g, '');
 }
 
 function moveSection(event){
@@ -28,14 +43,18 @@ function moveSection(event){
 
     currentActiveSection.classList.remove('active_block');
     nextSection.classList.add('active_block');
+    if(nextSectionNum === '4') {
+        showCalorie(calorieCalculation(userData));
+    }
 }
 
-function showRangeValue(querySelector){
-    var heightSpan = document.querySelector(querySelector);
-    heightSpan.innerText = `${currentHeight.value} см.`;
+function showHeightValue(querySelector){
+    var span = document.querySelector(querySelector);
+    span.innerText = `${currentHeight.value} см.`;
+    setHeight(currentHeight.value);
 }
 
-function getGenderValue(event){
+function setGenderValue(event){
     var gender = event.target.getAttribute('data-name');
     var result;
 
@@ -45,32 +64,100 @@ function getGenderValue(event){
             break;
         case 'two':
             result = 'female';
+            break
+        default:
+            result = 'male';
     }
-    return result;
+
+    userData.gender = result;
 }
 
-function getWeight(event){
+function setHeight(currentHeight){
+    userData.height = currentHeight;
+}
+
+function setWeight(event){
     var weight = event.target.value;
-    console.log(weight);
+    userData.weight = weight;
+}
+
+function setWishfulWeight(event){
+    userData.wishfulWeight = event.target.value;
+}
+
+function setActivity(activity){
+    userData.activity = activity;
+}
+
+function setWishfulResult(value){
+    userData.wishfulResult = value;
 }
 
 function showActivity(event){
     var value = event.target.value;
-    var img = document.querySelector("img");
+    var img = document.querySelector(".active_block img");
     var span = document.querySelector('span[name=activity]');
     switch (value) {
         case '1':
-            span.innerText = 'Нет физической нагрузки';
+            span.innerText = 'Слабая';
             img.setAttribute('src', './img/sit.jpg');
             break;
         case '2':
-            span.innerText = 'Тренировки несколько раз в неделю';
+            span.innerText = 'Умеренная';
             img.setAttribute('src', './img/fewtimes.jpg');
             break;
         case '3':
-            span.innerText = 'Ежедневные тренировки';
+            span.innerText = 'Интенсиваня';
             img.setAttribute('src', './img/everyday.jpg');
             break;
     }
+    setActivity(value);
 }
+
+function showWishfulResult(event){
+    var value = event.target.value;
+    var img = document.querySelector(".active_block img");
+    var span = document.querySelector('span[name=wish-result]');
+    switch (value) {
+        case '1':
+            span.innerText = 'Похудеть';
+            img.setAttribute('src', './img/tiny.jpg');
+            break;
+        case '2':
+            span.innerText = 'Набрать мышечную массу';
+            img.setAttribute('src', './img/muscle.jpg');
+            break;
+    }
+    setWishfulResult(value);
+}
+
+function calorieCalculation(userData){
+    var gender = userData.gender ? userData.gender : 'male';
+    var height = userData.height;
+    var weight = userData.weight;
+    var wishfulWeight = userData.wishfulWeight;
+    var activityIndex = userData.activity;
+    var wishfulResult = userData.wishfulResult;
+    var result;
+
+    if (gender === 'male') result = (10 * weight + 6.25 * height + 5) * activityTable[activityIndex];
+    if(gender === 'female') result = (10 * weight + 6.25 * height - 161) * activityTable[activityIndex];
+    if(wishfulResult === '1') {
+        result -= 300;
+    } else if(wishfulResult === '2'){
+        result += 300;
+    }
+    wishfulWeight < weight ? result -= 100: result +=100;
+
+    return result;
+}
+
+function showCalorie(value){
+    var resultSpan = document.querySelector('.active_block span');
+    resultSpan.innerText = value;
+    console.log(userData);
+}
+
+
+
 
